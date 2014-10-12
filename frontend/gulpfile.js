63719edfbcf5d39ad0aae2233a407a3d7e658c96
Peter de Croos <cultofmetatron@aumlogic.com>
2014-10-11 17:30:40 -0700

    added a gulp-serve task

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -3,10 +3,9 @@ var less = require('gulp-less');
 var path = require('path');
 var jsx = require('gulp-jsx');
 var browserify = require('gulp-browserify');
+var _ = require('lodash');
 
-
-
-gulp.task('build', function() {
+gulp.task('javascript', function() {
   return gulp.src('src/js/app.js')
     .pipe(browserify({
       transform: ["reactify"],
@@ -14,10 +13,38 @@ gulp.task('build', function() {
     .pipe(gulp.dest('./public/js'));
 });
 
-gulp.task('less', function () {
+gulp.task('less', function() {
   return gulp.src('./src/styles/stylesheet.less')
     .pipe(less())
     .pipe(gulp.dest('./public/styles'));
 });
 
-gulp.task('default', ['less']);
+var templates = [
+  'index'
+];
+
+gulp.task('html', function() {
+  return gulp.src(_.map(templates, function(template) {
+    return './src/templates/' + template + '.html'
+  }))
+  .pipe(gulp.dest('./public'));
+})
+
+gulp.task('fonts', function() {
+  return gulp.src('./src/fonts/**/*')
+  .pipe(gulp.dest('./public/fonts'))
+})
+
+//watcher
+
+
+//server
+var serve = require('gulp-serve');
+gulp.task('serve', serve({
+  root: 'public',
+  port: 8080
+})); 
+
+gulp.task('default', ['less', 'html', 'javascript']);
+
+
