a2991d37836264476218a3d49ceca74feb366304
Peter de Croos <cultofmetatron@aumlogic.com>
2014-11-16 14:00:26 -0800

    got state working

--- frontend/src/js/views/menu.jsx
+++ frontend/src/js/views/menu.jsx
@@ -7,6 +7,9 @@ var links = [
   {name: 'about', src:"/about"}
 ];
 
+var mui = require('material-ui');
+var PaperButton = mui.PaperButton;
+
 var moment = require('moment')
 
 var menu = React.createClass({
@@ -25,8 +28,8 @@ var menu = React.createClass({
   render: function() {
     return (
       <div>
-        <h1>TimePolice!</h1>
-        <h2>{this.state.date}</h2>
+        <h1 className="{this.state.date}">TimePolice!</h1>
+        <PaperButton type="RAISED" label={ "Time:  " + this.state.date}></PaperButton>
       </div>
     );
   }
